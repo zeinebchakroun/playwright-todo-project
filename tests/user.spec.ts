364b9ca260039362a36f0e82d069b419dd33946c
zeinebchakroun <chakrounzeineb.pro@gmail.com>
2026-05-18 17:43:55 +0100

    fix : filename case sensitivity

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,7 +1,6 @@
 
 import {test, expect} from '@playwright/test';
-import {faker} from '@faker-js/faker';
-import User from '../models/user';
+import User from '../models/User';
 import RegisterPage from '../pages/RegisterPage';
 import TodoPage from '../pages/TodoPage';
 
